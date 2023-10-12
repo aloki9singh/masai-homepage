8db84678c9b4c21272bde4a1be69dcb260aae6af
ajayahara <abak00350@gmail.com>
2023-10-12 21:52:11 +0530

    career tech section completed

--- masai-homepage/src/lib/CareerTech.svelte
+++ masai-homepage/src/lib/CareerTech.svelte
@@ -14,13 +14,12 @@
         </span>
         To Launch Your Career In Tech
     </h1>
-    <div class='w-full'>
-        <div class="w-1/6 flex flex-col gap-10 relative">
+    <div class="w-11/12 mt-6 flex gap-5 mx-auto">
+        <div class="w-2/6 flex flex-col gap-10 relative text-sm">
             <Button
-                size="xl"
                 outline
                 color="red"
-                class="hover:bg-white hover:text-red-500 p-2 bg-white flex justify-between"
+                class="hover:bg-white hover:text-red-500 p-2 bg-white flex justify-start w-3/4"
             >
                 <span
                     class="h-12 w-12 bg-red-200 flex justify-center items-center mr-3 text-gray-900 rounded-md text-md"
@@ -38,10 +37,9 @@
                 </span>
             </Button>
             <Button
-                size="xl"
                 outline
                 color="dark"
-                class="hover:bg-white hover:text-red-500 p-2  bg-white flex justify-between"
+                class="hover:bg-white hover:text-red-500 p-2  bg-white flex justify-start w-3/4"
             >
                 <span
                     class="h-12 w-12 bg-gray-200 flex justify-center items-center mr-3 text-gray-900 rounded-md text-md"
@@ -59,17 +57,16 @@
                 </span>
             </Button>
             <Button
-                size="xl"
                 outline
                 color="dark"
-                class="hover:bg-white hover:text-red-500 p-2  bg-white flex justify-between "
+                class="hover:bg-white hover:text-red-500 p-2  bg-white flex justify-start w-3/4"
             >
                 <span
                     class="h-12 w-12 bg-gray-200 flex justify-center items-center mr-3 text-gray-900 rounded-md text-md"
                     >3</span
                 >
                 <span
-                    >Practice<span class="relative">
+                    >Practice <span class="relative">
                         Based
                         <img
                             alt="yellow-vector"
@@ -80,10 +77,9 @@
                 </span>
             </Button>
             <Button
-                size="xl"
                 outline
                 color="dark"
-                class="hover:bg-white hover:text-red-500 p-2  bg-white flex justify-between"
+                class="hover:bg-white hover:text-red-500 p-2  bg-white flex justify-start w-3/4"
             >
                 <span
                     class="h-12 w-12 bg-gray-200 flex justify-center items-center mr-3 text-gray-900 rounded-md text-md"
@@ -101,10 +97,9 @@
                 </span>
             </Button>
             <Button
-                size="xl"
                 outline
                 color="dark"
-                class="hover:bg-white hover:text-red-500 p-2  bg-white flex justify-between"
+                class="hover:bg-white hover:text-red-500 p-2  bg-white flex justify-start w-3/4"
             >
                 <span
                     class="h-12 w-12 bg-gray-200 flex justify-center items-center mr-3 text-gray-900 rounded-md text-md"
@@ -121,8 +116,49 @@
                     </span>
                 </span>
             </Button>
-            <span class="absolute border-l-2 z-[-1] border-dotted border-black w-full h-full top-0 left-8"></span>
+            <span
+                class="absolute border-l-2 z-[-1] border-dotted border-black w-full h-full top-0 left-8"
+            />
         </div>
-        <div />
+        <div class="w-4/6 flex flex-col shadow-lg bg-gray-100 p-3 rounded-xl">
+            <p class="text-xl font-bold text-gray-600 text-center">Lunch A Sustainable Career</p>
+            <div class="flex gap-5 w-full py-5">
+                <div class="w-full  h-full">
+                    <iframe src="https://player.vimeo.com/video/867868566?title=0&amp;byline=0&amp;portrait=0&amp;playsinline=0&amp;autopause=0&amp;app_id=122963" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" title="Cab Driver became Software Engineer – Meet Ashish Raj from Munger, Bihar (1)" data-ready="true" class='rounded-xl w-full h-[300px]'></iframe>
+                </div>
+                <div class="w-1/2 flex flex-col gap-4">
+                    <div class="w-full flex justify-center items-center bg-[#1a9fbd] p-4 rounded-2xl">
+                        <div class='text-3xl font-extrabold text-[#ffdb66] mr-2'>₹6.9 LPA</div>
+                        <div class="text-xl text-white font-semibold uppercase">
+                            <p>Average</p>
+                            <p>Ctc</p>
+                        </div>
+                    </div>
+                    <div class="w-full flex justify-center items-center bg-[#1a9fbd] p-4 rounded-2xl">
+                        <div class='text-3xl font-extrabold text-[#ffdb66] mr-2'>3500+</div>
+                        <div class="text-xl text-white font-semibold uppercase">
+                            <p>Career</p>
+                            <p>Lunched</p>
+                        </div>
+                    </div>
+                    <div class="w-full flex justify-center items-center bg-[#1a9fbd] p-4 rounded-2xl">
+                        <div class='text-3xl font-extrabold text-[#ffdb66] mr-2'>4000+</div>
+                        <div class="text-xl text-white font-semibold uppercase">
+                            <p>Hiring</p>
+                            <p>Partners</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-between">
+                <img width="150px" height="150px" class="rounded-sm" src="https://www.masaischool.com/images/new-homepage/outcome/dream11.png" alt="alt">
+                <img width="150px" height="150px" class="rounded-sm" src="https://www.masaischool.com/images/new-homepage/outcome/global-logic.png" alt="alt">
+                <img width="150px" height="150px" class="rounded-sm" src="https://www.masaischool.com/images/new-homepage/outcome/uber.png" alt="alt">
+                <img width="150px" height="150px" class="rounded-sm" src="https://www.masaischool.com/images/new-homepage/outcome/paytm.png" alt="alt">
+                <img width="150px" height="150px" class="rounded-sm" src="https://www.masaischool.com/images/new-homepage/outcome/coin-switch.png" alt="alt">
+                <img width="150px" height="150px" class="rounded-sm" src="https://www.masaischool.com/images/new-homepage/outcome/share-chat.png" alt="alt">
+                <img width="150px" height="150px" class="rounded-sm" src="https://www.masaischool.com/images/new-homepage/outcome/ola.png" alt="alt">
+            </div>
+        </div>
     </div>
 </div>
